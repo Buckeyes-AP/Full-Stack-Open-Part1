f27b51815ce5ffa4e14e23afbe9c3bfca3862e0c
Alex Poirier <alexmatthewpoirier@gmail.com>
2023-02-22 16:18:04 -0500

    Finished 1.2

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,6 +1,6 @@
 
 const Header = (props) => {
-  console.log(props);
+  //console.log(props);
   return (
     <div>
       <h1>{props.course}</h1>
@@ -9,19 +9,49 @@ const Header = (props) => {
   );
 };
 
+const Part1 = (props) => {
+  //console.log(props);
+  return (
+    <div>
+      <p>{props.part1} {props.exercises1}</p>
+    </div>
+  )
+}
+
+const Part2 = (props) => {
+  //console.log(props);
+  return (
+    <div>
+      <p>{props.part2} {props.exercises2}</p>
+    </div>
+  )
+}
+
+const Part3 = (props) => {
+  //console.log(props);
+  return (
+    <div>
+      <p>{props.part3} {props.exercises3}</p>
+    </div>
+  )
+}
+
+
+
+
 const Content = (props) => {
   console.log(props);
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part1 part1={props.part1} exercises1={props.exercises1}/>
+      <Part2 part2={props.part2} exercises2={props.exercises2}/>
+      <Part3 part3={props.part3} exercises3={props.exercises3}/>
     </div>
   );
 };
 
 const Total = (props) => {
-  console.log(props);
+  //console.log(props);
   return (
     <div>
       <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
@@ -46,6 +76,7 @@ const App = () => {
       <Content part2={part2} exercises2={exercises2}/>
       <Content part3={part3} exercises3={exercises3}/>
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+
     </div>
   );
   
