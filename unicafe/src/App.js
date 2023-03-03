28a97a74aef473ff52aa1fa3db955fd2ef8dd069
Alex Poirier <alexmatthewpoirier@gmail.com>
2023-03-03 12:27:17 -0500

    finished 1.7

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -4,17 +4,54 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+
+  const increaseGood = () => {
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    const updatedTotal = neutral + bad + updatedGood;
+    setTotal(updatedTotal);
+    const updatedAverage = (updatedGood - bad) / updatedTotal;
+    setAverage(updatedAverage);
+    const updatePositive = (updatedGood / updatedTotal) * 100;
+    setPositive(updatePositive);
+  }
+  const increaseNeutral = () => {
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    const updatedTotal = good + bad + updatedNeutral;
+    setTotal(updatedTotal);
+    const updatePositive = (good / updatedTotal) * 100;
+    setPositive(updatePositive);
+    // setAverage((good - bad) / total);
+  }
+  const increaseBad = () => {
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    const updatedTotal = good + neutral + updatedBad;
+    setTotal(updatedTotal);
+    const updatedAverage = (good - updatedBad) / updatedTotal;
+    setAverage(updatedAverage);
+    const updatePositive = (good / updatedTotal) * 100;
+    setPositive(updatePositive);
+    // setAverage((good - updatedBad) / total)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={increaseGood}>good</button>
+      <button onClick={increaseNeutral}>neutral</button>
+      <button onClick={increaseBad}>bad</button>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   )
 }
